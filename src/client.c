af2f08987c6d2f8a467251891ec6ffc129f51156
Matheus Pissaia <pissaia.matheus03@gmail.com>
2025-05-01 19:48:17 -0300

    refactor: client and rooms (WIP)

--- src/client.c
+++ src/client.c
@@ -1,54 +1,50 @@
 #include <pthread.h>
+#include <stdlib.h>
 
 #define MAX_CLIENTS 10
 
 typedef struct
 {
-    int socket;
-    pthread_t thread;
+    int socket_id;
 } Client;
 
+int clients_count = 0;
 Client clients[MAX_CLIENTS];
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 // TODO implement
 void *handle_client(void *arg);
 
-Client *get_client(int *client_socket)
+Client *get_client(int *client_id)
 {
     for (int i = 0; i < MAX_CLIENTS; i++)
-    {
-        if (clients[i].socket == *client_socket)
-        {
+        if (clients[i].socket_id == *client_id)
             return &clients[i];
-        }
-    }
 }
 
-Client *add_client(int *client_socket)
+Client *add_client(int *client_id)
 {
-    // TODO lock and unlock mutex here?
     pthread_mutex_lock(&clients_mutex);
 
-    Client *new_client;
+    Client new_client;
 
-    new_client->socket = *client_socket;
-    // TODO thread id should be initialized as current clients length
-    // new_client->thread = 0;
-
-    pthread_create(new_client->thread, NULL, handle_client, &new_client);
+    new_client.socket_id = *client_id;
+    clients[clients_count] = new_client;
+    clients_count++;
 
     pthread_mutex_unlock(&clients_mutex);
+
+    return &new_client;
 };
 
-void remove_client(int *client_socket)
+// TODO implement
+void remove_client(int *client_id);
+
+// Should be executed in a separate thread to simulate different clients
+// connecting to the server.
+void *create_fake_client()
 {
-    // TODO lock and unlock mutex here?
-    pthread_mutex_lock(&clients_mutex);
+    int client_id = rand();
 
-    Client *client = get_client(client_socket);
-    pthread_cancel(client->thread);
-    close(client->socket);
-
-    pthread_mutex_unlock(&clients_mutex);
+    add_client(&client_id);
 };
--- src/room.c
+++ src/room.c
@@ -6,7 +6,7 @@
 typedef struct
 {
     int id;
-    Client clients[MAX_CLIENTS_PER_ROOM];
+    Client *clients[MAX_CLIENTS_PER_ROOM];
     int client_count;
     pthread_mutex_t mutex;
     pthread_cond_t cond;
@@ -21,7 +21,7 @@ Room *get_room(const int *room_id)
             return &rooms[i];
 }
 
-int join_room(const int *room_id, Client client)
+int join_room(const int *room_id, Client *client)
 {
     Room *room = get_room(room_id);
 
@@ -38,4 +38,24 @@ int join_room(const int *room_id, Client client)
     }
 
     return -1;
+}
+
+int leave_room(const int *room_id, Client *client)
+{
+    Room *room = get_room(room_id);
+
+    pthread_mutex_lock(&room->mutex);
+
+    for (int i = 0; i < room->client_count; i++)
+        if (room->clients[i] == client)
+        // TODO We should use a linked list, since this duplicates items.
+        {
+            room->clients[i] = room->clients[room->client_count - 1];
+            room->client_count--;
+            break;
+        }
+
+    pthread_mutex_unlock(&room->mutex);
+
+    return 0;
 }
