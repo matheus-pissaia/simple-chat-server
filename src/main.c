734efa0da4302762c7e0fec2170da969c795114e
Matheus Pissaia <pissaia.matheus03@gmail.com>
2025-05-07 18:04:25 -0300

    refactor: remove unnecessary stuff

--- src/main.c
+++ src/main.c
@@ -1,26 +1,9 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-
-#include "client.c"
-#include "message.c"
+#include "room.h"
 
 int main()
 {
-    int server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_len = sizeof(struct sockaddr_in);
+    init_rooms();
 
-    // server_socket = socket(...);
-    // bind(server_socket, ...);
-    // listen(server_socket, ...);
-
-    while (1)
-    {
-        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len);
-
-        add_client(&client_socket);
-    }
+    return 0;
 }
