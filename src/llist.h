b28c1eac1d9f2fec2bc8dbfe821ee80b19d4e917
Matheus Pissaia <pissaia.matheus03@gmail.com>
2025-05-12 18:13:54 -0300

    docs: update linked list docs

--- src/llist.h
+++ src/llist.h
@@ -3,20 +3,47 @@
 
 typedef struct Node
 {
-    void *data;
-    struct Node *next;
+    void *data;        // Pointer to the data
+    struct Node *next; // Pointer to the next node
 } Node;
 
 typedef struct
 {
-    int size;
-    Node *head;
-    Node *tail;
+    int size;   // Size of the linked list
+    Node *head; // Pointer to the head node
+    Node *tail; // Pointer to the tail node
 } LinkedList;
 
+/**
+ * @brief Creates a new linked list.
+ */
 LinkedList *llist_create();
+
+/**
+ * @brief Pops the first node from the linked list and returns it.
+ *
+ * @param llist Pointer to the linked list.
+ *
+ * @return Pointer to the popped node.
+ */
 Node *llist_pop(LinkedList *llist);
+
+/**
+ * @brief Pushes a new node with the given data to the end of the linked list.
+ *
+ * @param llist Pointer to the linked list.
+ * @param data Pointer to the data to be added.
+ */
 void llist_push(LinkedList *llist, void *data);
+
+/**
+ * @brief Removes the first node with the given data from the linked list.
+ *
+ * @param llist Pointer to the linked list.
+ * @param data Pointer to the data to be removed.
+ *
+ * @return 0 on success, -1 if the data was not found.
+ */
 int llist_remove(LinkedList *llist, void *data);
 
 #endif
